7760e94f11af8230de365e3d5445af23981c6e9b
jslopes <jslopes@uoldiveo.com>
2021-04-29 09:30:02 -0300

    fix main

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_efs_file_system" "main" {
 resource "aws_efs_mount_target" "main" {
     count   = var.enabled && length(var.subnet_id) > 0 ? length(var.subnet_id) : 0
 
-    file_system_id      = aws_efs_file_system.efs-data.id
+    file_system_id      = aws_efs_file_system.main.id
     subnet_id           = element(var.subnet_id, count.index)
     security_groups     = var.security_groups
 }
